bbc6a5d99af3785ffa1f642b0c65eba31f487b55
freecodecampvideo <freecodecampvideo@gmail.com>
2022-04-12 22:19:59 -0300

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,28 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+from web3 import Web3
+
+
+def deploy_fund_me():
+    account = get_account()
+    # pass priceFeed address to our fundme contract
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        price_feed_address = deploy_mocks()
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
+
+
+def main():
+    deploy_fund_me()
